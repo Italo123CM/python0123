b578b0906a9678d3032a9609426ec990820ba3cc
Rodrigo Italo Cayo Mamani <122212749+Italo123CM@users.noreply.github.com>
2023-02-15 14:41:00 -0500

    Update migrate.py

--- project/migrate.py
+++ project/migrate.py
@@ -1,16 +1,20 @@
+
 import sqlite3
 
-conn=sqlite3.connect('tienda.db')
+conn=sqlite3.connect('PRECIO.db')
 cursor_obj = conn.cursor()
 cursor_obj.execute("DROP TABLE IF EXISTS USUARIOS")
-table = """ CREATE TABLE USUARIOS (
+table = """ CREATE TABLE PRECIOS_DOLAR (
             ID  INTEGER PRIMARY KEY AUTOINCREMENT,
-            USUARIO VARCHAR(25),
-            PASSWORD VARCHAR(255) NOT NULL,
-            EMAIL VARCHAR(255) NOT NULL,
-            FULLNAME VARCHAR(25) NOT NULL,
-            SCORE INT,
-            TIPOUSUARIO VARCHAR(25)
+            COMPRA VARCHAR(9),
+            VENTA VARCHAR(9),
+            FECHA VARCHAR(25) ,
+            LOCAL_DE_CAMBIO VARCHAR(25)
+        );"""
+cursor_obj.execute(table)  
+#ESTA PARTE ES PARA CERRAR EL ARCHIVO DE BASE DE DATOS
+conn.commit()
+
         ); """
 cursor_obj.execute(table)
 cursor_obj.execute("DROP TABLE IF EXISTS PRODUCTOS")
